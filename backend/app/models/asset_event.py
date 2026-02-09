7d1fb46da8185c0c7cfdc4ed6e5e086c432085f7
msfxinfinity <101818211+msfxinfinity@users.noreply.github.com>
2026-02-09 13:37:22 +0300

    add asset event model.

---
+++ backend/app/models/asset_event.py
@@ -0,0 +1,45 @@
+from sqlalchemy import Column,  Integer, String, DateTime, Enum, ForeignKey
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.declarative import declarative_base
+from datetime import datetime
+import enum
+
+Base = declarative_base()
+
+class AssetEventType(enum.Enum):
+    CREATED = "created"
+    UPDATED = "updated"
+    ARCHIVED = "archived"
+    DELETED = "deleted"
+
+class AssetEvent(Base):
+    __table_name__ = "asset_events"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+
+    asset_id = Column(
+        Integer,
+        ForeignKey("assets.id"),
+        nullable=False,
+        index=True
+    )
+
+    tenant_id = Column(
+        Integer,
+        ForeignKey("tenants.id"),
+        nullable=False,
+        Index=True
+    )
+
+    event_type = Column(
+        Enum(AssetEventType),
+        nullable=False
+    )
+
+    user_id = Column(Integer,nullable=False)
+    user_role = Column(String, nullable=False)
+
+    geolocation = Column(JSONB, nullable=False)
+
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+
